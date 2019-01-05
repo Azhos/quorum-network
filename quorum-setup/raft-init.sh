4a0d5e24eecac9858ef75c6ca5e59eeeb4f7abc6
baermathias <45003352+baermathias@users.noreply.github.com>
2019-01-05 23:24:19 +0100

    Update raft-init.sh

--- quorum-setup/raft-init.sh
+++ quorum-setup/raft-init.sh
@@ -8,6 +8,7 @@ mkdir -p qdata/logs
 echo "[*] Configuring node"                       
 mkdir -p qdata/{keystore,geth}
 cp raft/static-nodes.json qdata
+cp raft/static-nodes.json qdata/permissioned-nodes.json
 cp raft/keystore/$(ls raft/keystore) qdata/keystore/acckey
 cp raft/geth/nodekey qdata/geth/nodekey
 geth --datadir qdata init genesis.json
